1a1fb35f0cd0b5443fe2980ea7a9c92e625d4d63
Ryan <jrnabayra14@gmail.com>
2023-07-15 22:36:09 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.{html,js}", "index.html"],
+  content: ["./dist/*.{html,js}", "index.html", "dist/pages/dishes.html"],
 
   theme: {
     extend: {
